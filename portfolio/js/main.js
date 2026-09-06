b649614a2a464cb9261eb53870096e3bac659458
sinhzhu <mightytran4@gmail.com>
2026-09-06 00:47:37 -0700

    Delete portfolio directory

--- portfolio/js/main.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const email = document.getElementById("contact-email");
-  if (email) {
-    const parts = ["mightytran4", "@", "gmail", ".", "com"];
-    const addr = parts.join("");
-    email.href = "mailto:" + addr;
-    email.textContent = addr;
-  }
-})();
