6aed42693d9458dfaf6719c1fa42b854c4ce614c
sinhzhu <mightytran4@gmail.com>
2026-09-06 21:34:27 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,9 +6,8 @@
     email.href = "mailto:" + addr;
     email.textContent = addr;
   }
-
-  document.querySelectorAll('a[href^="http"]').forEach((a) => {
-    a.target = "_blank";
-    a.rel = "noopener noreferrer";
-  });
 })();
+document.querySelectorAll('a[href^="http"]').forEach((a) => {
+  a.target = "_blank";
+  a.rel = "noopener noreferrer";
+});
